8e0f87a5f5749bed3dcea6e327b42267081f8441
Bohdan186 <lbogdan186@gmail.com>
2021-03-30 09:23:50 +0300

    30.03 project1 - performed a couple of tasks

--- projects/03-2021/project-1/js/main.js
+++ projects/03-2021/project-1/js/main.js
@@ -1,152 +1,298 @@
-console.log('Task 1');
+console.log('====================Task 1');
 
 function strToArray(str) {
-	let result = str.split(' ');
-	return result;
+	return str.split(' ');
 }
 
 console.log(strToArray('Lorem ipsum dolor sit amet consectetur'));
 
-console.log('Task 2');
+
+console.log('====================Task 2');
 
 function numbSymbol(str) {
-	let result = str.length;
-	return result;
+	return str.length;
 }
 
 console.log(numbSymbol('Lorem ipsum dolor sit amet consectetur'));
 
-console.log('Task 3');
+
+console.log('====================Task 3');
 
 function changeSep(str, delimiter) {
-	let result = str.split(' ').join(delimiter);
-	return result;
+	return str.split(' ').join(delimiter);
 }
 
 console.log(changeSep('Lorem ipsum dolor sit amet consectetur', '-'));
 
-console.log('Task 4');
 
-function strToUpperCase(str) {
-	let result = str.toUpperCase();
-	return result;
+console.log('====================Task 4');
+
+function firstSymbolToUpperCase(str) {
+	array = str.split(' ');
+
+	for (let i = 0; i < array.length; i++) {
+		array[i] = array[i].charAt(0).toUpperCase() + array[i].slice(1).toLowerCase();
+	}
+	
+	return array.join(' ')
 }
 
-console.log(strToUpperCase('Lorem ipsum dolor sit amet consectetur'));
+console.log(firstSymbolToUpperCase('Lorem ipsum dolor sit amet consectetur'));
 
-console.log('Task 5');
+
+console.log('====================Task 5');
 
 function compareStr(str1, str2) {
-	let result = (str1.toUpperCase() === str2.toUpperCase());
-	return result;
+	return str1.toUpperCase() === str2.toUpperCase();
 }
 
 console.log(compareStr('Lorem ipsum dolor sit amet consectetur','Lorem ipsum dolor SIT AMET consectetur'));
 
-console.log('Task 6');
+
+console.log('====================Task 6');
 
 function searchStrInStr(str, searchStr) {
-	str = str.toUpperCase();
-	searchStr = searchStr.toUpperCase();
-
-	let result = str.split(' ').indexOf(searchStr);
-
-	return result != -1 ? true : false;
+	return str.toUpperCase().split(' ').indexOf(searchStr.toUpperCase()) != -1;
 }
 
-console.log(searchStrInStr('Lorem ipsum dolor sit amet consectetur', 'ipsuM')); 
-
-// console.log('Task 7');
-
-// function strToCamelCase(str) {
-
-// 	return str.toUpperCase().split(/\s+/).map(word => word[0].toLowerCase() + word.substring(1)).join(" ")
-
-	
-// }
-
-// console.log(strToCamelCase('Lorem ipsum dolor')); 
+console.log(searchStrInStr('Lorem ipsum dolor sit amet consectetur', 'IpsuM')); 
 
 
-console.log('Task 8');
+console.log('====================Task 7');
+
+function strToCamelCase(str) {
+	array = str.split(' ');
+
+	for (let i = 0; i < array.length; i++) {
+		array[i] = array[i].charAt(0).toUpperCase() + array[i].slice(1).toLowerCase();
+	}
+
+	return  array.join('').charAt(0).toLowerCase() +  array.join('').slice(1)
+}
+
+console.log(strToCamelCase('LOREM     IPSUM dolor')); 
+
+
+console.log('====================Task 8');
 
 function toSnakeCase(str, delimiter) {
-	let result = str.split(' ').join(delimiter);
-	return result;
+	return str.split(' ').join(delimiter);
 }
 
 console.log(toSnakeCase('Lorem ipsum dolor sit amet consectetur', '_'));
 
 
-console.log('Task 9');
+console.log('====================Task 9');
 
 function repeatWord(word, number) {
-	
 	for (let i = 0; i < number; i++) {
 		console.log(word);
 	}
 	
 }
 
-console.log(repeatWord('Lorem', 5));
+repeatWord('Lorem', 5);
 
 
-console.log('Task 10');
+console.log('====================Task 10');
 
 function partStr(str, symbol) {
-	let result = str.substr(0, str.indexOf(symbol));
-	return result;
-	
+	return str.substr(0, str.indexOf(symbol));
 }
 
 console.log(partStr('Lorem ipsum dolor sit ! amet consectetur', '!'));
 
-console.log('Task 11');
+
+console.log('====================Task 11');
 
 function cutString(str, number) {
 	let result = [];
-	  for(var i = 0 ; i < str.length; i += number) {
+
+	for(var i = 0 ; i < str.length; i += number) {
 		result.push(str.substr(i, number));
-	  }
-	  return result;
 	}
+
+	return result;
+}
 	
 console.log(cutString('Lorem-ipsum-dolor',3));
 
 
-console.log('Task 12');
+console.log('====================Task 12');
 
 function numberSymbol(str) {
-	let result = str.split(' ').join('').length;
-	  return result;
+	  return str.split(' ').join('').length;
 	}
 	
 console.log(numberSymbol('Lorem     aaa'));
 
 
-console.log('Task 13');
+console.log('====================Task 13');
 
 function sortWord(str) {
-	let result = str.split('').sort().join('');
-	  return result;
+	  return str.split('').sort().join('');
 	}
 	
 console.log(sortWord('Lorem'));
 
 
-// console.log('Task 14');
+console.log('====================Task 14');
 
-// function removeDuplicate(str) {
+function removeDuplicate(str) {
+	let array = str.split(', ');
+	let result = [];
 
-// 	let arr = str.split(', ');
+	for (let i = 0; i < array.length; i++) {
+		result.indexOf(array[i]) == -1 ? result.push(array[i]) : {} ;
+	}
 
-// 	arr.forEach(element => {
-// 		if (element )
-// 	});
-
-
-
-// 	return result;
-// }
+	return result;
+}
 	
-// console.log(removeDuplicate('вишня, груша, слива, груша'));
+console.log(removeDuplicate('вишня, груша, слива, груша'));
+
+
+
+console.log('====================Task 15');
+
+function firstSymbolToLowerCase(str) {
+	array = str.split(' ');
+
+	for (let i = 0; i < array.length; i++) {
+		array[i] = array[i].charAt(0).toLowerCase() + array[i].slice(1).toUpperCase();
+	}
+	
+	return array.join(' ');
+}
+
+console.log(firstSymbolToLowerCase('Lorem ipsum dolor sit amet consectetur'));
+
+
+console.log('====================Task 16');
+
+function whatType(param) {
+	return typeof(param);
+}
+
+console.log(whatType('Lorem'));
+
+
+console.log('====================Task 17');
+
+function lengthStringArray(array) {
+	let len = 0;
+	let result;
+
+	for (let i = 0; i < array.length; i++) {
+		if(array[i].length > len) {
+			len = array[i].length;
+			result = array[i];
+		}			
+	}
+
+	return result;
+}
+
+console.log(lengthStringArray(['Lorem', 'Lorem Lorem', 'Lorem Lorem Lorem Lorem Lorem Lorem', 'ss']));
+
+
+console.log('====================Task 18');
+
+function lengthWordInStr(str) {
+	let len = 0;
+	let result;
+
+	array = str.split(' ');
+
+	for (let i = 0; i < array.length; i++) {
+		if(array[i].length > len) {
+			len = array[i].length;
+			result = array[i];
+		}			
+	}
+
+	return result;
+}
+
+console.log(lengthWordInStr('Lorem ipsum dolor sit amet consectetur'));
+
+
+console.log('====================Task 19');
+
+function numberWordsInStr(str) {
+	
+	let array = str.split(' ');
+
+	let result = 0;
+
+	for (let i = 0; i <= array.length; i++) {
+		result = i;		
+	}
+
+	return result;
+}
+
+console.log(numberWordsInStr('Lorem ipsum dolor sit amet consectetur'));
+
+
+console.log('====================Task 20');
+
+function changeDateFormat(date) {
+	let array = date.split('-')
+	let result = [];
+
+	result.push(array[2]);
+	result.push(array[1]);
+	result.push(array[0]);
+
+	return result.join('.');
+
+}
+
+console.log(changeDateFormat('31-12-2030'));
+
+
+console.log('====================Task 21');
+
+function combineArrays(array1, array2) {
+	return array1.concat(array2);
+}
+
+console.log(combineArrays([1,2,3], [4,5,6]));
+
+
+console.log('====================Task 22');
+
+function addElementToArray(array, addEl) {
+	array.push(addEl);
+	return array;
+}
+
+console.log(addElementToArray([1,2,3], 42));
+
+
+console.log('====================Task 23');
+
+function orderArray(array, order) {
+	return order === 'normal' ? array : array.reverse();
+}
+
+console.log(orderArray([1,2,3], 'normal'));
+
+
+console.log('====================Task 24');
+
+function removeElementInArray(array, remEl) {
+	return array.filter(el => el !== remEl);
+}
+
+console.log(removeElementInArray([1,2,3,42], 42));
+
+
+// console.log('====================Task 25');
+
+// function howOld(day, month, year) {
+	
+// }
+
+// console.log(howOld(20, 06, 1999));
