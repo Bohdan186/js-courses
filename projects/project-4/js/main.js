eae32c63ac51fda64e13abf599df2d693c35d38e
Bohdan186 <lbogdan186@gmail.com>
2021-04-07 16:49:02 +0300

    07.04 project4 - add cache

--- projects/project-4/js/main.js
+++ projects/project-4/js/main.js
@@ -1,44 +1,77 @@
-$.ajax('https://jsonplaceholder.typicode.com/users', {
-
-	success: function(data) {	
-		data.forEach(el => {
-			let id = el.id;
-			let name = el.name;
-			let company = el.company.name;
-
-			$('.users').append(
-				`<div class="user" data-user-id="${id}">
-					<div class="avatar"><img src="img/avatar.png" alt="avatar"></div>
-					<div class="content">
-						<h4>${name}</h4>
-						<p><small>${company}</small></p>
-					</div>
-				</div>`
-			);
-		});
-	}
-});
-
-$(document).on('click','.user', function(){
-	let $thisUser = $(this);
-	$('.post').remove();
-
-	$thisUser.siblings().removeClass('active');
-	$thisUser.addClass('active');
-
-	let userId = $thisUser.data('user-id');
-
-	$.ajax(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`, {
+function addUsersData() {
+	$.ajax({
+		url: 'https://jsonplaceholder.typicode.com/users',
+	
 		success: function(data) {
-			data.forEach(el => {
-				let id = el.id;
-				let title = el.title;
-				let body = el.body
-				$('.posts').append(
-					`<div class="post" data-post-id="${id}">
+			data.forEach(function(el) {
+	
+				$('.users').append(
+					`<div class="user" data-user-id="${el.id}">
+						<div class="avatar"><img src="img/avatar.png" alt="avatar"></div>
 						<div class="content">
-							<h2>${title}</h2>
-							<p>${body}</p>
+							<h4>${el.name}</h4>
+							<p><small>${el.company.name}</small></p>
+						</div>
+					</div>`
+				);
+			});
+		}
+	});
+}
+
+function addUserPost() {
+	let userPostData = {};
+	let userPostDataKeys = {};
+
+	$(document).on('click','.user', function(){
+		let $thisUser = $(this);
+		let userId = $thisUser.data('user-id');
+
+		if($thisUser.hasClass('active')){
+			return false;
+		};
+
+		$('.post').remove();
+		$thisUser.siblings().removeClass('active');
+		$thisUser.addClass('active');
+
+		if(!userPostData.hasOwnProperty(`user_id_${userId}`)) {
+			
+			$.ajax({
+				url: `https://jsonplaceholder.typicode.com/posts?userId=${userId}`,
+				
+				success: function(data) {
+					userPostData[`user_id_${userId}`] = new Array();
+
+					data.forEach(function(el) {
+						userPostData[`user_id_${userId}`].push(el);
+
+						for (let key in userPostData) {
+							userPostDataKeys[key] = key;
+						}
+
+						$('.posts').append(
+							`<div class="post" data-post-id="${el.id}">
+								<div class="content">
+									<h2>${el.title}</h2>
+									<p>${el.body}</p>
+									<div class="comments">
+										<a href=#>View comments</a>
+									</div>
+								</div>
+							</div>`
+						);
+					});
+				}
+			});
+		}else {
+			userPostData[`user_id_${userId}`].forEach(function(el) {
+
+				$('.posts').append(
+					`<div class="post" data-post-id="${el.id}">
+						<div class="content">
+							<h2>${el.title}</h2>
+							<p>${el.body}</p>
 							<div class="comments">
 								<a href=#>View comments</a>
 							</div>
@@ -47,29 +80,31 @@ $(document).on('click','.user', function(){
 				);
 			});
 		}
-	});
-})
+	})
+}
 
+addUsersData();
+addUserPost();
 
-$(document).on('click','.comments', function(){
-	let $thisComments = $(this);
-	let postId = $thisComments.data('post-id');
+// $(document).on('click','.comments', function(){
+// 	let $thisComments = $(this);
+// 	let postId = $thisComments.data('post-id');
 
-	$.ajax(`https://jsonplaceholder.typicode.com/comments?postId=${postId}`, {
-		success: function(data) {
-			data.forEach(el => {
-				let name = el.name;
-				let email = el.email;
-				let body = el.body;
+// 	$.ajax(`https://jsonplaceholder.typicode.com/comments?postId=${postId}`, {
+// 		success: function(data) {
+// 			data.forEach(el => {
+// 				let name = el.name;
+// 				let email = el.email;
+// 				let body = el.body;
 
-				$('.comments').append(
-					`<div class="content">
-						<h5>${name}</h5>
-						<div class="meta-data">${email}</div>
-						<p>${body}</p>
-					</div>`
-				);
-			});
-		}
-	});
-})
+// 				$('.comments').append(
+// 					`<div class="content">
+// 						<h5>${name}</h5>
+// 						<div class="meta-data">${email}</div>
+// 						<p>${body}</p>
+// 					</div>`
+// 				);
+// 			});
+// 		}
+// 	});
+// })
