1d6b63f139976084a10d3c8e953ceac900afaf51
Bohdan186 <lbogdan186@gmail.com>
2021-04-09 12:49:44 +0300

    09.04 project5 - add change unit button

--- projects/project-5/js/main.js
+++ projects/project-5/js/main.js
@@ -1,7 +1,69 @@
 (function($) {
+
+	function changeUnit(){
+		let unit;
+		$('.change-unit .btn').on('click', function() {
+			$(this).siblings().removeClass('active');
+			$(this).addClass('active');
+			unit = $(this).data('unit');
+		});
+		console.log(unit);
+		return unit;
+	}
+
+	function renderWeatherData(data){
+		$('.city-name').text(' ').append(data.name);
+		$('.temp').text(' ').append(data.main.temp);
+		$('.description').text(' ').append(data.weather[0].description);
+		if('clear sky' === data.weather[0].description){
+			$('.img-wrapper').append('<img src="img/clearSky.jpg"></img>');
+		}
+	}
+
+	function getWeatherData_geo(position){
+		let lat = position.coords.latitude;
+		let lon = position.coords.longitude;
+		let unit = $('.change-unit .btn').data('unit');
+
+		$.ajax({
+			url: `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=${unit}&appid=e3b0b4e9f227920605001124729511a0`,
 	
+			success: function(data){
+				renderWeatherData(data);
+			}
+		});
+	}
+
+	function getWeatherData_cityName(){
+		let cityName = $('.search-city').val();
+
+
+		let unit = $('.change-unit .active').data('unit');
+
+		console.log(unit);
+		
+		if(!cityName){
+			alert('Введіть назву міста!');
+		}else {
+			$.ajax({
+				url: `https://api.openweathermap.org/data/2.5/weather?q=${cityName},ua&units=${unit}&appid=e3b0b4e9f227920605001124729511a0`,
+		
+				success: function(data){
+					renderWeatherData(data);
+				}
+			});
+		}
+	}
 
 	$(document).ready(function() {
+		// navigator.geolocation.getCurrentPosition(getWeatherData_geo);
+
+		$('.change-unit .btn').on('click', function(){
+			$(this).siblings().removeClass('active');
+			$(this).addClass('active');
+		});
+		
+		$('.widget-search-button').on('click', getWeatherData_cityName);
 		
 	});
 })(jQuery);
