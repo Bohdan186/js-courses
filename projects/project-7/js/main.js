2f1603d61913e39389c5d5d44be1c8b1343ef649
Bohdan186 <lbogdan186@gmail.com>
2021-04-15 17:13:40 +0300

    15.04 project7 - add swiper and popup

--- projects/project-7/js/main.js
+++ projects/project-7/js/main.js
@@ -1,38 +1,85 @@
-$('.products').each(function() {
-	let $thisProduct = $(this);
-	let $tabsList = $thisProduct.find('.tabs-menu > li');
+(function($) {
+	$('.products').each(function() {
+		let $thisProduct = $(this);
+		let $tabsList = $thisProduct.find('.tabs-menu > li');
 
-	$tabsList.on('click', function(e) {
-		e.preventDefault();
-		let $thisTab = $(this);
-		let tabsCategory = $thisTab.data('category');
-		let $activeCategory = $thisProduct.find(`.content-list > div[data-category=${tabsCategory}]`);
+		$tabsList.on('click', function(e) {
+			e.preventDefault();
+			let $thisTab = $(this);
+			let tabsCategory = $thisTab.data('category');
+			let $activeCategory = $thisProduct.find(`.content-list > div[data-category=${tabsCategory}]`);
 
-		$thisTab.siblings().removeClass('active');
-		$thisTab.addClass('active');
-	
-		$activeCategory.siblings().removeClass('active');
-		$activeCategory.addClass('active');
+			$thisTab.siblings().removeClass('active');
+			$thisTab.addClass('active');
+		
+			$activeCategory.siblings().removeClass('active');
+			$activeCategory.addClass('active');
+		});
 	});
-});
 
-const swiper = new Swiper('.swiper-container', {
-	// Optional parameters
-	direction: 'horizontal',
-	loop: true,
+	function getSwiperSettings(object){
+		let userSwiperSetting = object.data('options');
+		let config = {};
 
-	// If we need pagination
-	pagination: {
-		el: '.swiper-pagination',
-		clickable: true,
-		renderBullet: function (index, className) {
-			return '<span class="' + className + '">' + (index + 1) + '</span>';
-		},
-	},
+		if(userSwiperSetting.direction){
+			config['direction'] = userSwiperSetting.direction;
+		}
 
-	// Navigation arrows
-	navigation: {
-		nextEl: '.next',
-		prevEl: '.prev',
-	},
-});
+		if(userSwiperSetting.loop){
+			config['loop'] = userSwiperSetting.loop;
+		}
+
+		if(userSwiperSetting.pagination){
+			config['pagination'] = {
+				el: '.swiper-pagination',
+				clickable:true,
+				bulletActiveClass:'active',
+				bulletClass:'slider-pagination-items',
+				renderBullet:function (index, className) {
+					return '<span class="' + className + '">' + (index + 1) + '</span>';
+				}
+			};
+		}
+
+		if(userSwiperSetting.navigation){
+			config['navigation'] = {
+				prevEl:'.prev',
+				nextEl:'.next'
+			}
+		}
+
+		if(userSwiperSetting.slidesPerView){
+			let slideViewNumb = userSwiperSetting.slidesPerView;
+			config['slidesPerView'] = 1;
+			
+			config['breakpoints'] = {
+				768: {
+					slidesPerView: Math.floor(slideViewNumb/2)
+				},
+
+				992: {
+				slidesPerView: slideViewNumb
+				}
+			};
+		}
+
+		if(userSwiperSetting.spaceBetween){
+			config['spaceBetween'] = userSwiperSetting.spaceBetween;
+		}
+
+		console.log(config);
+		return config;
+	}
+
+	$(document).ready(function(){
+		$('.swiper-container').each(function(){
+			var $this = $(this);
+			new Swiper($this[0], getSwiperSettings($this));
+		});
+		
+		$('.show-popup').magnificPopup({
+			type: 'image'
+		});
+	});
+
+})(jQuery);
