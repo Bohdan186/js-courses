b0eb9bf8b3a92aba7bd4a69a908becf26d65bd78
Bohdan186 <lbogdan186@gmail.com>
2021-04-19 17:04:01 +0300

    19.04

--- projects/project-7/js/main.js
+++ projects/project-7/js/main.js
@@ -1,30 +1,34 @@
 (function($) {
 
-	$('.products').each(function() {
-		let $thisProduct = $(this);
-		let $tabsList = $thisProduct.find('.tabs-menu > li');
-
-		$tabsList.on('click', function(e) {
-			e.preventDefault();
-			let $thisTab = $(this);
-			let tabsCategory = $thisTab.data('category');
-			let $activeCategory = $thisProduct.find(`.content-list > div[data-category=${tabsCategory}]`);
-
-			$thisTab.siblings().removeClass('active');
-			$thisTab.addClass('active');
-		
-			$activeCategory.siblings().removeClass('active');
-			$activeCategory.addClass('active');
+	function initTabs() {
+		$('.products.tabs').each(function() {
+			let $thisProduct = $(this);
+			let $tabsList = $thisProduct.find('.products.tabs .tabs-menu > li');
+	
+			$tabsList.on('click', function(e) {
+				e.preventDefault();
+				let $thisTab = $(this);
+				let tabsCategory = $thisTab.data('category');
+				let $activeCategory = $thisProduct.find(`.products.tabs .content-list > div[data-category=${tabsCategory}]`);
+	
+				$thisTab.siblings().removeClass('active');
+				$thisTab.addClass('active');
+			
+				$activeCategory.siblings().removeClass('active');
+				$activeCategory.addClass('active');
+			});
 		});
-	});
+	}
 
-	function initSwiper(object){
-		if(undefined === typeof Swiper){
+	function initSwiper() {
+		if(undefined === typeof Swiper) {
 			return false;
 		}
+
+		let $object = $('.swiper-container');
 		
-		function getSwiperSettings(object){
-			let userSwiperSetting = object.data('options');
+		function getSwiperSettings($object) {
+			let userSwiperSetting = $object.data('options');
 			let config = {
 				loop: userSwiperSetting.loop,
 				breakpoints: {
@@ -38,11 +42,11 @@
 
 					992: {
 						slidesPerView: userSwiperSetting.desktop
-					}
+					},
 				},
 			};
 
-			if(userSwiperSetting.pagination){
+			if(userSwiperSetting.pagination) {
 				config['pagination'] = {
 					el: '.swiper-pagination',
 					clickable:true,
@@ -53,41 +57,43 @@
 					}
 				};
 			}
-			if(userSwiperSetting.navigation){
+
+			if(userSwiperSetting.navigation) {
 				config['navigation'] = {
 					prevEl:'.prev',
-					nextEl:'.next'
+					nextEl:'.next',
 				}
 			}
-			if(userSwiperSetting.spaceBetween){
+
+			if(userSwiperSetting.spaceBetween) {
 				config['spaceBetween'] = userSwiperSetting.spaceBetween;
 			}
 
 			return config;
 		}
 
-		object.each(function(){
+		$object.each(function() {
 			$thisSwiper = $(this);
 			new Swiper($thisSwiper[0], getSwiperSettings($thisSwiper));
 		});
 	}
 
-	function initPopup(object){
-		if(undefined === typeof $.fn.magnificPopup){
+	function initPopup() {
+		if(undefined === typeof $.fn.magnificPopup) {
 			return false;
 		}
 
-		object.magnificPopup({
+		$('.show-popup').magnificPopup({
 			type:'inline',
 		});
 	}
 
-	function initWaypoint(object){
-		if(undefined === typeof Waypoint){
+	function initWaypoint() {
+		if(undefined === typeof Waypoint) {
 			return false;
 		}
 
-		object.each(function(){
+		$('.with-animated').each(function() {
 			let $this = $(this);
 
 			new Waypoint({
@@ -96,13 +102,13 @@
 					$this.addClass('animate');
 				},
 
-				offset: '70%'
+				offset: '70%',
 			})
 		});
 	}
 
-	function initAutocomplete(object){
-		if(undefined === typeof $.fn.autocomplete){
+	function initAutocomplete() {
+		if(undefined === typeof $.fn.autocomplete) {
 			return false;
 		}
 
@@ -121,25 +127,59 @@
 			{ value: 'Shop'},			
 			{ value: 'Blog'},
 			{ value: 'About us'},			
-			{ value: 'Contact us'}
+			{ value: 'Contact us'},
 		];
-		
-		$('.autocomplete-suggestions').css('max-height: initial');
 
-		object.autocomplete({
+		$('.search-products').autocomplete({
 			lookup: countries,
+			
 			onSelect: function (suggestion) {
 				console.log(suggestion);
-			}
+			},
 		});
-		
+	}
+
+	function initTooltip() {
+		if(undefined === typeof $.fn.tooltip) {
+			return false;
+		}
+
+		$('[data-toggle="tooltip"]').tooltip();
+	}
+
+	function initFilter() {
+		if(undefined === typeof Isotope) {
+			return false;
+		}
+
+		let $grid = new Isotope('.products.filter .content-list .row', {
+			itemSelector: '.filter-grid',
+			layoutMode: 'fitRows',
+		});
+
+		$('.tabs-menu').on('click', 'li', function(e) {
+			e.preventDefault();
+
+			let $this = $(this);
+			let filterData = $this.data('category');
+
+			$this.siblings().removeClass('active');
+			$this.addClass('active');
+			
+			$grid.arrange({
+				filter: filterData,
+			});
+		});
 	}
 
 	$(document).ready(function(){
-		initSwiper($('.swiper-container'));
-		initPopup($('.show-popup'));
-		initWaypoint($('.with-animated'));
-		initAutocomplete($('.search-products'));
+		initTabs();
+		initSwiper();
+		initPopup();
+		initWaypoint();
+		initAutocomplete();
+		initTooltip();
+		initFilter();
 	});
 
 })(jQuery);
